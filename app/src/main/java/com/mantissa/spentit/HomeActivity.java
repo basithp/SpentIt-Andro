d449297186a354f1c904cf1a4ae869aab62ee171
basithp <basithp@gmail.com>
2016-09-19 17:53:49 +0530

    Making a crash

--- app/src/main/java/com/mantissa/spentit/HomeActivity.java
+++ app/src/main/java/com/mantissa/spentit/HomeActivity.java
@@ -29,6 +29,7 @@ public class HomeActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                        AssertNotNull(null);
             }
         });
 
